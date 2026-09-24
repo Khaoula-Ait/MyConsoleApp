7b56111dd572e8218282251115a1003b2e54e659
Khaoula ait <akaoula34@gmail.com>
2026-09-24 20:56:30 +0100

    Adding welcome message

--- Program.cs
+++ Program.cs
@@ -2,20 +2,15 @@
 {
     internal class Program
     {
+        static void welcome(string name)
+        {
+            Console.WriteLine($"Welcome {name} !!");
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World from C# App");
-            Console.WriteLine("Hello, World from The B1 branch");
-            Console.WriteLine("C1 + main 1");
-            Console.WriteLine("C1 + main 2"); 
-            Console.WriteLine("C1 + main 3");
-            Console.WriteLine("C1 + main 4");
-            Console.WriteLine("C1 + main 5");
-            Console.WriteLine("C1 + main 6");
-            Console.WriteLine("C1 + main 7");
-            Console.WriteLine("C1 + main 8"); 
-            Console.WriteLine("C1 + main 9"); 
-            Console.WriteLine("C1 + main 10");
+
+            welcome("Git");
 
         }
     }
