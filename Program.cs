757e72d916bebb442a731af4a4752c8aefcba9e1
Khaoula ait <akaoula34@gmail.com>
2026-09-24 20:41:14 +0100

    C1 Done

--- Program.cs
+++ Program.cs
@@ -6,16 +6,16 @@
         {
             Console.WriteLine("Hello, World from C# App");
             Console.WriteLine("Hello, World from The B1 branch");
-            Console.WriteLine("1");
-            Console.WriteLine("2"); 
-            Console.WriteLine("3");
-            Console.WriteLine("4");
-            Console.WriteLine("5");
-            Console.WriteLine("6");
-            Console.WriteLine("7");
-            Console.WriteLine("8"); 
-            Console.WriteLine("9"); 
-            Console.WriteLine("10");
+            Console.WriteLine("C1  1");
+            Console.WriteLine("C1  2"); 
+            Console.WriteLine("C1  3");
+            Console.WriteLine("C1  4");
+            Console.WriteLine("C1  5");
+            Console.WriteLine("C1  6");
+            Console.WriteLine("C1  7");
+            Console.WriteLine("C1  8"); 
+            Console.WriteLine("C1  9"); 
+            Console.WriteLine("C1  10");
 
         }
     }
