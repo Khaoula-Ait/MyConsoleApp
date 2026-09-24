aa3b2178523909e2cba20f6d81a6fc082af9d66a
Khaoula ait <akaoula34@gmail.com>
2026-09-24 20:20:53 +0100

    Second Branch Done

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World from C# App");
+            Console.WriteLine("Hello, World from The second branch");
         }
     }
 }
