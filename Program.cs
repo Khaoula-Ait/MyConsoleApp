811ffd36bb0590be70ebca68ce1223c7f0ab1aec
Khaoula ait <akaoula34@gmail.com>
2026-09-24 20:38:18 +0100

    B1 Completed

--- Program.cs
+++ Program.cs
@@ -5,7 +5,18 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World from C# App");
-            Console.WriteLine("Hello, World from The second branch");
+            Console.WriteLine("Hello, World from The B1 branch");
+            Console.WriteLine("1");
+            Console.WriteLine("2"); 
+            Console.WriteLine("3");
+            Console.WriteLine("4");
+            Console.WriteLine("5");
+            Console.WriteLine("6");
+            Console.WriteLine("7");
+            Console.WriteLine("8"); 
+            Console.WriteLine("9"); 
+            Console.WriteLine("10");
+
         }
     }
 }
